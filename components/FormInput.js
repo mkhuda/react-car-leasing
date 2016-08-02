50f3f93c7f9c1e092e41d489e6cbf6df4ba42a69
mkhuda <mhmmd.huda@gmail.com>
2016-08-02 14:27:34 +0700

    fix / remove react-dom findDomNode

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import actions from '../redux/actions'
-import CurrencyInput from 'react-currency-input';
+import CurrencyInput from 'react-currency-input'
 import Select from 'react-select'
 
 class FormInput extends Component {
@@ -17,10 +17,8 @@ class FormInput extends Component {
   }
 
   handleChange(event) {
-    const price = ReactDOM.findDOMNode(this.refs.price).value.trim();
-    const dp = ReactDOM.findDOMNode(this.refs.dp).value.trim();
-    console.log(price);
-    console.log(dp);
+    const price = this.refs.price.value
+    const dp = this.refs.price.value
     this.setState({
       inputPrice: price,
       inputDownPayment: dp
@@ -28,23 +26,21 @@ class FormInput extends Component {
   }
 
   selectChange(val) {
-    const price = ReactDOM.findDOMNode(this.refs.price).value.trim();
-    const dp = ReactDOM.findDOMNode(this.refs.dp).value.trim();
     this.setState({
-      inputPrice: price,
-      inputDownPayment: dp,
       inputSelectYear: val.value
     })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    const price = ReactDOM.findDOMNode(this.refs.price).value.trim();
-    const dp = ReactDOM.findDOMNode(this.refs.dp).value.trim();
-    const year = this.state.inputSelectYear
+    const input_price = this.refs.price
+    const value_price = input_price.value
+    const input_dp = this.refs.dp
+    const value_dp = input_dp.value
+    const value_year = this.state.inputSelectYear
 
     // this.props.addTodo(text)
-    this.props.calculateNow(price, dp, year)
+    this.props.calculateNow(value_price, value_dp, value_year)
   }
 
   render() {
@@ -67,16 +63,16 @@ class FormInput extends Component {
             ref="price"
             defaultValue={this.state.inputPrice}
             className="inputText"
-
           />
+
           <input
             type="text"
             placeholder="Down Payment (Rp.)"
             ref="dp"
             defaultValue={this.state.inputDownPayment}
             className="inputText"
-
           />
+
           <Select
             name="form-field-name"
             value={this.state.inputSelectYear}
@@ -84,9 +80,11 @@ class FormInput extends Component {
             options={options}
             onChange={this.selectChange.bind(this)}
           />
+
           <div className="divButton">
             <input className="mainButton" type="submit" value="Calculate Now"/>
           </div>
+
         </form>
       </div>
     )
